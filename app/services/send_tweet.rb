3cbe0584b129f6ff80c8b61499722bf385c1a643
TinaHelfer <34861185+TinaHelfer@users.noreply.github.com>
2018-02-15 21:09:21 +0100

    Update send_tweet.rb

--- app/services/send_tweet.rb
+++ app/services/send_tweet.rb
@@ -3,10 +3,10 @@ require 'Twitter'
 class SendTweet
   def log_in_to_twitter
   @client = Twitter::REST::Client.new do |config|
-    config.consumer_key        = "yI136QFevxLzjMxwGIXrzmwnM"
-    config.consumer_secret     = "EhUJL8nQ33CHJ7RcxFJDmeZe7Si6aAkS7gv9j8o8vSqDdERyMR"
-    config.access_token        = "953578324189736960-dGr3w7de8QYhay60ssQCP0i5rSPQ3RI"
-    config.access_token_secret = "pNwQ1IQhwMvhOEVa7nC6hkdB22Es1mf4lg4xXDTg1M7oc"
+    config.consumer_key        = ""
+    config.consumer_secret     = ""
+    config.access_token        = ""
+    config.access_token_secret = ""
   end
   p "login: #@client"
   end
